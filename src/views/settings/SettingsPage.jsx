3fcb5ada04dc57645eafb8652b6b0edd09465235
Sebastian Escobar <carsebastian1013@gmail.com>
2025-04-03 23:29:59 -0500

    dev version (0.0.0.4)

--- src/views/settings/SettingsPage.jsx
+++ src/views/settings/SettingsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewInput from '../../components/aux/NewInput';
 import { Link } from 'react-router-dom';
 
 const SettingsPage = () => {
@@ -6,6 +7,8 @@ const SettingsPage = () => {
     <div className="page-wrapper">
         <h1>Ajustes</h1>
         <p>Configura opciones generales, preferencias de usuario y temas.</p>
+        <NewInput label="Nombre de Usuario" />
+        <br />
         <Link to="/" className="btn btn-primary">
             <button>Ir a Inicio</button>
         </Link>
